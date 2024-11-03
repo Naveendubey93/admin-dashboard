21a331d23d91d5031050586e8a4c7e2673f91a0e
Naveen Dubey <naveen.parasi@gmail.com>
2024-11-03 21:58:22 +0530

    added protected routes

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,19 @@
+import {  Navigate, Outlet } from 'react-router-dom'
+import { useAuthStore } from '../store'
+
+const Dashboard = () => {
+
+const { user } = useAuthStore();
+if(!user) {
+  return <Navigate to="/auth/login" replace={true} />;
+}
+
+  return (
+    <div>
+      <h1>Dashboard Component</h1>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Dashboard
