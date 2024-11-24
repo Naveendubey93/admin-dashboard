48aa0d1fde8f7a79bb01887ec93d921315d018b2
Naveen Dubey <naveen.parasi@gmail.com>
2024-11-24 16:26:54 +0530

    added logout functionality

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,7 +1,7 @@
 import {  Navigate, NavLink, Outlet } from 'react-router-dom'
 import { useAuthStore } from '../store'
-import {  Layout, Menu, theme } from 'antd';
-import Icon, {  UserOutlined } from '@ant-design/icons'; 
+import {  Avatar, Badge, Dropdown, Flex, Layout, Menu, Space, theme } from 'antd';
+import Icon, {  BellFilled, UserOutlined } from '@ant-design/icons'; 
 
 import Sider from 'antd/es/layout/Sider';
 import { Content, Footer, Header } from 'antd/es/layout/layout';
@@ -11,6 +11,8 @@ import Home from '../components/icons/Home';
 import { foodIcon } from '../components/icons/FoodIcon';
 import BasketIcon from '../components/icons/BasketIcon';
 import GiftIcon from '../components/icons/GiftIcon';
+import { useMutation } from '@tanstack/react-query';
+import { logout } from '../http/api';
 const items= [
   {
     key: '/',
@@ -40,6 +42,15 @@ const items= [
 
 ];
 const Dashboard = () => {
+  const { logout: logoutFromStore} = useAuthStore();
+  const {mutate: logoutMutate } = useMutation({
+  mutationKey: ['logout'],
+  mutationFn: logout,
+  onSuccess: async () => {
+    logoutFromStore();
+    // return;
+  }
+  });
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer },
@@ -53,26 +64,50 @@ if(!user) {
 
   return (
     <div>
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider 
-      theme='light'
-      collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="logo">
-          <Logo/>
-          </div>
-        <Menu theme="light" defaultSelectedKeys={['/']} mode="inline" items={items} />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '0 16px' }}>
-        <Outlet/>
+      <Layout style={{ minHeight: '100vh', background: colorBgContainer }}>
+        <Sider 
+        theme='light'
+        collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+          <div className="logo">
+            <Logo/>
+            </div>
+          <Menu theme="light" defaultSelectedKeys={['/']} mode="inline" items={items} />
+        </Sider>
+        <Layout>
+          <Header style={{ paddingLeft: '16px', paddingRight: '16px', background: colorBgContainer}}>
+          <Flex gap="middle" align="start" justify='space-between'>
+            <Badge text="Global" status='success'/>
+              <Space size={16}>
+                <Badge dot={true}>
+                  <BellFilled/>
+               
+                </Badge>
+                   <Dropdown menu={{ items: [
+                    { key: 'logout', label: 'Logout', onClick:() => logoutMutate(),}
+                  ] }} placement='bottomRight'>
+                    <Avatar style={{ backgroundColor: '#fde3cf', color: '#f56a00' }}>U</Avatar>
 
+                  </Dropdown>
+              </Space>
+          </Flex>
+          </Header>
+            <Content style={{margin: '0 16px'}}>
+          <Outlet/>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-            Fullstack test application       
-         </Footer>
-      </Layout>
-    </Layout>      
+          Fullstack test application       
+        </Footer>
+        </Layout>
+      
+        {/* <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+            <Content style={{ margin: '0 16px' }}>
+            <Outlet/>
+
+            </Content>
+          
+        </Layout> */}
+      </Layout>      
     </div>
   )
 }
